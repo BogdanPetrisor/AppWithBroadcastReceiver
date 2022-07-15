e85ca62df4987a1038634e7dccca3a8514c7fec1
Petrisor Bogdan <bogdan.petrisor@yopeso.com>
2022-07-15 18:27:27 +0300

    Create an IntentFilter which register an AirPlaneModeChangedReceiver

--- app/src/main/java/com/example/appwithbroadcastreceiver/MainActivity.kt
+++ app/src/main/java/com/example/appwithbroadcastreceiver/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.appwithbroadcastreceiver
 
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    lateinit var receiver: AirPlaneModeChangedReceiver
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        receiver = AirPlaneModeChangedReceiver()
+        IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED).also {
+            registerReceiver(receiver, it)
+        }
     }
+
 }
